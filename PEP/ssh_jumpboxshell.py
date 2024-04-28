23925e5b6998e75ceae876824c66d51661875d6f
Your Name <you@example.com>
2024-04-28 17:01:22 +0800

    PDP

--- PEP/ssh_jumpboxshell.py
+++ PEP/ssh_jumpboxshell.py
@@ -1,103 +1,131 @@
-import paramiko
-import threading
-import socket
-import sys
-import subprocess
-import termios
-import select 
-import requests
+import paramiko                 
+import threading                
+import socket                  
+import sys                      
+import subprocess               
+import termios                  
+import select                   
+import requests                 
+import os
+import rsa 
+
+# 生成RSA金鑰對
+def generate_rsa_keypair():
+    (public_key , privkey) = rsa.newkeys(1024)
+        
+    private_key_path = os.path.expanduser("/root/.ssh/id_rsa")
+    public_key_path = os.path.expanduser("/root/.ssh/id_rsa.pub")
+    
+    # 保存私鑰到文件
+    pub = public_key.save_pkcs1()
+    pubfile = open(private_key_path , 'wb')
+    pubfile.write(pub)
+    pubfile.close()
+    
+    # 保存公鑰到文件
+    pri = privkey.save_pkcs1()
+    prifile = open(public_key_path , 'wb')
+    prifile.write(pri)
+    prifile.close()
+
+    print("RSA key pair generated successfully.")
+
+    return private_key_path, public_key_path
+
 
 # 伺服器的 RSA 金鑰
 host_key = paramiko.RSAKey.generate(2048)
 
+
 # 定義一個用戶名和密碼的驗證函數
 def check_auth(username, password):
-    # 在這裡你可以添加你的身份驗證邏輯，例如從資料庫中檢查用戶名和密碼
-    # 這個例子中，只是簡單地固定了一組用戶名和密碼
-    
     r = requests.post("http://192.168.166.16:3000/", data = {'username' : 'pep' , 'password' : 'pep'})
     print(r.text)
     print("身分驗證成功")
     return username == 'pep' and password == 'pep'
 
+
 # 定義一個 SSH 伺服器類別
 class SSHServer(paramiko.ServerInterface):
     def __init__(self):
         self.event = threading.Event()
-    # 通道request
+
     def check_channel_request(self, kind, chanid):
         if kind == 'session':
             return paramiko.OPEN_SUCCEEDED
         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
-
-    # 帳號密碼確認
+        
     def check_auth_password(self, username, password):
         if check_auth(username, password):
             return paramiko.AUTH_SUCCESSFUL
         return paramiko.AUTH_FAILED
-    # shell request
+    
     def check_channel_shell_request(self, channel):
         return True
+    
+    def add_public_key(self, public_key_path):
+        try:
+            with open(public_key_path, "r") as public_key_file:
+                public_key = public_key_file.read().strip()
 
+            # 追加公钥到 authorized_keys 文件
+            with open("/root/.ssh/known_hosts", "a") as authorized_keys_file:
+                authorized_keys_file.write(public_key + "\n")
+
+            print("公钥添加成功")
+        except Exception as e:
+            print("添加公钥失败:", e)
+    
 # 建立一個 SSH 伺服器
 try:
-    # 初始化 SSH 伺服器
     ssh_server = SSHServer()
-    
-    # 建立一個 socket 並綁定到指定的port
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(('0.0.0.0', 2223))
-    server_socket.listen(100)
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)           
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)        
+    server_socket.bind(('0.0.0.0', 2223))                                       
+    server_socket.listen(100)                                                   
     print("等待連接......")
-    
-    # 等待並處理客戶端的連接
+
     client_socket, client_addr = server_socket.accept()
     print(f"與 {client_addr} 建立連接")
 
-    # 初始化傳輸層
-    transport = paramiko.Transport(client_socket)
-    transport.add_server_key(host_key)
-    transport.start_server(server=ssh_server)
+    transport = paramiko.Transport(client_socket)                               
+    transport.add_server_key(host_key)                                         
+    transport.start_server(server=ssh_server)   
 
-    # 等待連接建立
-    chan = transport.accept(20)
-    if chan is None:
+    chan = transport.accept(20)                                                 
+    if chan is None:                                                            
         print('*** No channel.')
-        sys.exit(1)
+        sys.exit(1)                                                             
+    print('[+] 連接成功！')  
 
-    print('[+] 連接成功！')
-
-    # 開啟 shell
     chan.send('\r\nWelcome to My SSH Server!\r\n')
 
-    # Jump Box 
+    # 生成 RSA 密鑰對並獲取公鑰路徑
+    private_key_path, public_key_path = generate_rsa_keypair()
 
-    # 建立一个socket
+    # 發送公鑰到服務器
+    ssh_server.add_public_key(public_key_path)
+
+
     trans = paramiko.Transport(('192.168.166.11', 22))
-    # 啟動客戶端
     trans.start_client()
-    # 帳號密碼登入
     trans.auth_password(username='resource', password='resource')
-    # 打開通道
     channel = trans.open_session()
-    # 取得終端
     channel.get_pty()
-    # xshell 終端
     channel.invoke_shell()
 
+    oldtty = termios.tcgetattr(sys.stdin)                                        
+    readlist, writelist, errlist = select.select([channel, sys.stdin,], [], []) 
 
-    oldtty = termios.tcgetattr(sys.stdin)
-    readlist, writelist, errlist = select.select([channel, sys.stdin,], [], [])
+    channel.send("\r")                                                           
+    result = channel.recv(8192)                                                  
     channel.send("\r")
     result = channel.recv(8192)
     channel.send("\r")
     result = channel.recv(8192)
-    channel.send("\r")
-    result = channel.recv(8192)
-    chan.send(result)
-    
-    
+    chan.send(result)         
+
     while True:
         try:
             command = chan.recv(1024)
@@ -122,13 +150,10 @@ try:
            #  output = subprocess.check_output(command.decode(), shell=True)
             
         except Exception as e:
-            chan.send(str(e))       
+            chan.send(str(e))     
 
-    # 關閉連接
-   # chan.close()
-   # transport.close()
-   # server_socket.close()
 
 except Exception as e:
     print("無法建立伺服器:", e)
     raise
+  
